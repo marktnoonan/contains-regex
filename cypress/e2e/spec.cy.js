78c753bb6a6ebafdb6672dc28e4d22a1c4a77100
marktnoonan <mark@cypress.io>
2023-02-23 15:00:13 -0500

    update with more examples

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,24 +1,27 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('will pass', () => {
+describe('page', {
+  defaultCommandTimeout: 100
+}, () => {
+  it('passes with cy.contains() and well-formed table', () => {
     cy.visit('valid-table.html')
 
+    cy.contains('ul figcaption', 'hello')
     const theRegex = new RegExp(`${Cypress._.escapeRegExp('DBU.VR.1')}`)
-    cy.contains('DBU.VR.1').should('be.visible') // passes 
-    cy.contains(theRegex).should('be.visible') // passes
-    cy.contains('a', theRegex).should('be.visible') // passes
-    cy.contains('[data-cy=table_aandachtspunten]', theRegex).should('be.visible') // passes
-    cy.contains('[data-cy=table_aandachtspunten] tr', theRegex).should('be.visible') // fails
-    cy.contains('[data-cy=table_aandachtspunten] tr a', theRegex).should('be.visible') // fails
+    cy.contains('DBU.VR.1').should('be.visible') 
+    cy.contains(theRegex).should('be.visible')
+    cy.contains('a', theRegex).should('be.visible')
+    cy.contains('[data-cy=table_aandachtspunten]', theRegex).should('be.visible')
+    cy.contains('[data-cy=table_aandachtspunten] tr', theRegex).should('be.visible')
+    cy.contains('[data-cy=table_aandachtspunten] tr a', theRegex).should('be.visible')
   })
 
-  it('some will fail', () => {
+  it('some will fail on a malformed table', () => {
     cy.visit('invalid-table.html')
 
     const theRegex = new RegExp(`${Cypress._.escapeRegExp('DBU.VR.1')}`)
     cy.contains('DBU.VR.1').should('be.visible') 
     cy.contains(theRegex).should('be.visible')
-    cy.contains('a', theRegex).should('be.visible') 
+    cy.contains('a', theRegex).should('be.visible')
     cy.contains('[data-cy=table_aandachtspunten]', theRegex).should('be.visible')
     cy.contains('[data-cy=table_aandachtspunten] tr', theRegex).should('be.visible') 
     cy.contains('[data-cy=table_aandachtspunten] tr a', theRegex).should('be.visible') 
@@ -34,5 +37,17 @@ describe('page', () => {
     cy.contains('[data-cy=table_aandachtspunten]', theString).should('be.visible')
     cy.contains('[data-cy=table_aandachtspunten] tr', theString).should('be.visible') 
     cy.contains('[data-cy=table_aandachtspunten] tr a', theString).should('be.visible') 
+  })
+  it('fails the same with cy.get()', () => {
+    cy.visit('invalid-table.html')
+    cy.get('[data-cy=table_aandachtspunten]').should('be.visible')
+    cy.get('[data-cy=table_aandachtspunten] tr').should('be.visible') 
+    cy.get('[data-cy=table_aandachtspunten] tr a').should('be.visible') 
+  })
+  it('passes with cy.get() on well-formed table', () => {
+    cy.visit('valid-table.html')
+    cy.get('[data-cy=table_aandachtspunten]').should('be.visible')
+    cy.get('[data-cy=table_aandachtspunten] tr').should('be.visible') 
+    cy.get('[data-cy=table_aandachtspunten] tr a').should('be.visible') 
   })
 })
