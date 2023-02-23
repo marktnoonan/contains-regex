75a806b12f7c81b7acbfd571f039a2dcb41b48c9
marktnoonan <mark@cypress.io>
2023-02-23 11:08:01 -0500

    tweak regex, add more passing/failing cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,10 +3,12 @@ describe('page', () => {
   it('works', () => {
     cy.visit('index.html')
 
-    const theRegex = new RegExp(`^${Cypress._.escapeRegExp('DBU.VR.1')}$`)
+    const theRegex = new RegExp(`${Cypress._.escapeRegExp('DBU.VR.1')}`)
     cy.contains('DBU.VR.1').should('be.visible') // passes 
-    cy.contains(theRegex).should('be.visible') // fails
-    cy.contains('[data-cy=table_aandachtspunten] tr a', theRegex).should('be.visible') // fails
+    cy.contains(theRegex).should('be.visible') // passes
+    cy.contains('a', theRegex).should('be.visible') // passes
+    cy.contains('[data-cy=table_aandachtspunten]', theRegex).should('be.visible') // passes
     cy.contains('[data-cy=table_aandachtspunten] tr', theRegex).should('be.visible') // fails
+    cy.contains('[data-cy=table_aandachtspunten] tr a', theRegex).should('be.visible') // fails
   })
 })
